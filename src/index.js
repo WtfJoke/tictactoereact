185916f71549ba2a275ca60dfd5bf61bdb000bf8
Manuel <manuel.wessner@codecentric.de>
2019-12-09 07:44:37 +0100

    :recycle: :bug: Assign some functions to consts
    - Fix bug with wrong step number
    - Initialize Step directly (instead of a method)
    - Loops must die :skull:

--- src/index.js
+++ src/index.js
@@ -1,150 +1,142 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
+const Square = ({ onClick, value }) => {
+    return (
+        <button className="square" onClick={onClick}>
+            {value}
+        </button>
+    )
 }
 
-
 function Board(props) {
+    const renderSquare = i => {
+        return (
+            <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
+        )
+    }
 
-  function renderSquare(i) {
-    return <Square
-      value={props.squares[i]}
-      onClick={() => props.onClick(i)}
-    />;
-  }
-
-  return (
-    <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  );
-
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    )
 }
 
 function Game() {
+    function handleCLick(i) {
+        const newHistory = history.slice(0, state.stepNumber + 1)
+        const next = state.xIsNext
+        const current = newHistory[newHistory.length - 1]
+        const squares = current.squares.slice()
+        if (calculateWinner(squares) || squares[i]) {
+            return
+        }
+        squares[i] = next ? 'X' : 'O'
 
-  function handleCLick(i) {
-    const newHistory = history.slice(0, state.stepNumber + 1);
-    const next = state.xIsNext
-    const current = newHistory[newHistory.length -1]
-    const squares = current.squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
-      return;
+        updateState({
+            history: newHistory.concat([
+                {
+                    squares: squares,
+                },
+            ]),
+            stepNumber: newHistory.length,
+            xIsNext: !next,
+        })
     }
-    squares[i] = next ? 'X' : 'O';
 
-    updateState({
-      history: newHistory.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !next,
-    });
-  }
+    function jumpTo(step) {
+        updateState({
+            history: state.history,
+            stepNumber: step,
+            xIsNext: step % 2 === 0,
+        })
+    }
 
-  function jumpTo(step) {
-    updateState({
-      history: state.history,
-      stepNumber: step,
-      xIsNext: (step % 2) === 0
+    const [state, updateState] = useState({
+        history: [
+            {
+                squares: Array(9).fill(null),
+            },
+        ],
+        stepNumber: 0,
+        xIsNext: true,
     })
-  }
 
-  const [state, updateState] = useState(initializeState)
-  const history = state.history
-  const current = history[state.stepNumber]
-  const winner = calculateWinner(current.squares)
-  let status;
-  if (winner) {
-    status = `Winner: ${winner}`
-  } else {
-    status = `Next player: ${state.xIsNext ? 'X' : 'O'}`;
-  }
+    const history = state.history
+    const current = history[state.stepNumber]
+    const winner = calculateWinner(current.squares)
 
-  const moves = history.map((_, move) => {
-    const description = move ?
-      `Go to move #${move}` :
-      "Go to game start";
+    const status = winner
+        ? `Winner: ${winner}`
+        : `Next player: ${state.xIsNext ? 'X' : 'O'}`
+
+    const moves = history.map((_, moveIndex) => {
+        const description = moveIndex
+            ? `Go to move #${moveIndex}`
+            : 'Go to game start'
+
+        return (
+            <li key={moveIndex}>
+                <button onClick={() => jumpTo(moveIndex)}>{description}</button>
+            </li>
+        )
+    })
 
     return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
-
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board
-          squares={current.squares}
-          onClick={(i) => handleCLick(i)}
-        />
-      </div>
-      <div className="game-info">
-        <div>{status}</div>
-        <ol>{moves}</ol>
-      </div>
-    </div>
-  );
-}
-
-function initializeState(props) {
-  return {
-    history: [{
-      squares: Array(9).fill(null),
-    }],
-    stepNumber: 0,
-    xIsNext: true
-  }
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares={current.squares}
+                    onClick={i => handleCLick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    )
 }
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Game />, document.getElementById('root'))
 
 function calculateWinner(squares) {
-  const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < winningCombinations.length; i++) {
-    const [a, b, c] = winningCombinations[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    const winnerRow = winningCombinations
+        .filter(notWinning(squares))
+        .map(winningRow => squares[winningRow[0]])
+
+    return winnerRow[0]
+}
+
+function notWinning(squares) {
+    return ([a, b, c]) =>
+        squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
 }
